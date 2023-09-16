5ec3442dd5a621ec2f4bce1e725c30d686ffaf75
yuwon <yuwon0628@handong.ac.kr>
2023-09-16 23:13:25 +0900

    [feat] WordManager file

--- src/main/java/com/mycom/word/WordManager.java
+++ src/main/java/com/mycom/word/WordManager.java
@@ -11,7 +11,7 @@ public class WordManager {
     public int selectMenu() {
         System.out.print("""
                 *** 영단어 마스터 ***
-                ********************
+                *****************
                 1. 모든 단어 보기
                 2. 수준별 단어 보기
                 3. 단어 검색
@@ -20,19 +20,36 @@ public class WordManager {
                 6. 단어 삭제
                 7. 파일 저장
                 0. 나가기
-                ********************
-                => 원하는 메뉴는?\s""") ;
+                *****************
+                => 원하는 메뉴는?\s """) ;
         return s.nextInt() ;
     }
     public void start() {
+        wordCRUD.loadFile();
         while(true) {
-            int menu = selectMenu() ;
-            if(menu == 0) break ;
-            if(menu == 4) {
-                wordCRUD.addWord() ;
+            int menu = selectMenu();
+            if(menu == 0) break;
+
+            else if(menu == 1) {
+                wordCRUD.listAll();
             }
-            else if(menu ==1) {
-                wordCRUD.listAll() ;
+            else if(menu == 2) {
+                wordCRUD.searchLevel();
+            }
+            else if(menu == 3) {
+                wordCRUD.searchWord();
+            }
+            else if(menu == 4) {
+                wordCRUD.addWord();
+            }
+            else if(menu == 5) {
+                wordCRUD.updateWord();
+            }
+            else if(menu == 6) {
+                wordCRUD.deleteWord();
+            }
+            else if(menu == 7) {
+                wordCRUD.saveFile();
             }
         }
         System.out.println("프로그램 종료! 다음에 만나요~") ;
