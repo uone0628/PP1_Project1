10a55d81fc0f81e188097797ee3d25a50ef24bed
yuwon <yuwon0628@handong.ac.kr>
2023-09-16 23:12:07 +0900

    [feat] ICRUD file

--- src/main/java/com/mycom/word/ICRUD.java
+++ src/main/java/com/mycom/word/ICRUD.java
@@ -2,7 +2,8 @@ package com.mycom.word;
 
 public interface ICRUD {
     public Object add() ;
-    public int update(Object obj) ;
-    public int delete(Object obj) ;
-    public void selectOne(int id) ;
+    public void updateWord();
+    public void deleteWord();
+    public void saveFile();
+    public void loadFile();
 }
