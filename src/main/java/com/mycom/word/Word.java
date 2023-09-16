7eb3078efe024021296fb052de0733cb903be58d
yuwon <yuwon0628@handong.ac.kr>
2023-09-16 23:12:42 +0900

    [feat] Word file

--- src/main/java/com/mycom/word/Word.java
+++ src/main/java/com/mycom/word/Word.java
@@ -53,7 +53,7 @@ public class Word {
         String slevel = "" ;
         for(int i = 0; i < level ; i++) slevel += "*" ;
         String str = String.format("%-3s", slevel)
-                + String.format("%15s", word) + "  " + meaning ;
+                + String.format("%20s", word) + "  " + meaning ;
         return str ;
     }
 }
